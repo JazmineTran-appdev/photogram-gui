7c217bdf2af3b0317269f9e1a7cc39ca01044b5c
Jazmine Tran <jazminetra00@gmail.com>
2023-04-11 20:34:12 +0000

    delete photos works

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,26 @@ class PhotosController < ApplicationController
 
     render({ :template => "photo_templates/index.html.erb"})
   end
+
+  def show
+    # Params = {"path_id"=>"777"}
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id })
+    @the_photo = matching_photos.first
+
+    render({ :template => "photo_templates/show.html.erb"})
+  end
+
+  def baii
+    # params = {"toast_id"=>"785"}
+
+    the_id = params.fetch("toast_id")
+    matching_photos = Photo.where({ :id => the_id })
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    # render({ :template => "photo_templates/baii.html.erb"})
+    redirect_to("/photos")
+  end
 end
